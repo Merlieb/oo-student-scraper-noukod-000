5dad1f560d8609476ded01c02279422947501bb6
Merline Belizaire <merline@noukod.com>
2018-10-26 18:10:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,16 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    
+    srape_url = Nokogiri::HTML(open(index_url))
+    srape_url.css(".student-card a").map do |html_student|
+      {name: html_student.css("h4").text, location: html_student.css("p").text, profile_url: html_student.attribute("href").value}
+    end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
