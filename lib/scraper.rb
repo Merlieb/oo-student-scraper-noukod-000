b6650a41021bf6b5e389ee7eea540d16675477fe
Merline Belizaire <merline@noukod.com>
2018-10-26 18:11:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,16 +2,34 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  def self.scrape_index_page(index_url)
-    srape_url = Nokogiri::HTML(open(index_url))
-    srape_url.css(".student-card a").map do |html_student|
-      {name: html_student.css("h4").text, location: html_student.css("p").text, profile_url: html_student.attribute("href").value}
-    end
+  
+def self.scrape_index_page(index_url)
+   srape_url = Nokogiri::HTML(open(index_url))
+   srape_url.css(".student-card a").map do |html_student|
+     {name: html_student.css("h4").text, location: html_student.css("p").text, profile_url: html_student.attribute("href").value}
+   end
+  
+ end
 
-  end
+ def self.scrape_profile_page(profile_url)
+   student, reseaus = {}, [:linkedin, :twitter, :github]
+   Nokogiri::HTML(open(profile_url)).tap do |nokogiri|
+     links = nokogiri.css(".social-icon-container a").map {|a| a.attribute("href").value}
 
-  def self.scrape_profile_page(profile_url)
+     links.each do |link|
+       reseaus.each do |reseau|
+         if link.include?(reseau.to_s)
+           student[reseau] = link
+         end
+       end
+       student[:blog] = link if !student.values.include?(link)
+     end
 
-  end
+     student[:profile_quote] = nokogiri.css(".profile-quote").text if nokogiri.css(".profile-quote")
+     student[:bio] = nokogiri.css("div.bio-content.content-holder div.description-holder p").text if nokogiri.css("div.bio-content.content-holder div.description-holder p")
 
+   end
+   # binding.pry
+   student
+ end
 end
